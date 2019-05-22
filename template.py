453c32561070ed6820269481f24d75b5d9268d6b
corbe <corbe@biologie.ens.fr>
2019-05-22 14:45:33 +0200

    started work on virginie template

--- template.py
+++ template.py
@@ -1,5 +1,13 @@
+from bin.readEmail import GmailAPI
+
 
 class Template(object):
 
     def __init__(self, message):
-        """"""
+        """message = dict with keys 'body' 'subject' 'to' 'from' 'date' """
+        if message.get('from') == "virginie.bues@ens.fr":
+            self.render = virginie_template
+
+
+def virginie_template():
+    """"""
