45e86fccd25735f92ec366bf7ca53cde81141bc0
Solene WEILL <weill@biocpc12.ens.fr>
2019-05-22 11:35:22 +0200

    updated code

--- bin/readEmail.py
+++ bin/readEmail.py
@@ -4,6 +4,7 @@ from oauth2client import file, client, tools
 import base64
 import quopri
 import email
+from bs4 import BeautifulSoup
 
 SCOPES = 'https://www.googleapis.com/auth/gmail.readonly'
 
@@ -25,37 +26,50 @@ def main():
 
     else:
 
-        for message in messages[:2]:
+        for message in messages[:5]:
             msg = service.users().messages().get(userId='me', id=message['id'], format='raw').execute()
 
             # base 62 renvoie un string encodé, mais la fonction par défaut de python ne peut pas le décoder...
             msg_str = base64.urlsafe_b64decode(msg['raw'])
 
             mime_msg = email.message_from_string(msg_str.decode('iso-8859-1'))
-            if mime_msg.is_multipart():
-                part = mime_msg.get_payload()[0]
-                char_set = part['Content-Type'].split('charset=')[-1].split(';')[0].strip('"').lower()
-            else:
-                char_set = mime_msg['Content-Type'].split('charset=')[-1].split(';')[0].strip('"').lower()
 
-            if mime_msg.is_multipart():
-                body = mime_msg.get_payload()[0].get_payload()
-            else:
-                body = mime_msg.get_payload()
 
-            print(body)
-            print(char_set)
+            for parts in mime_msg.walk():
+                mime_msg.get_payload()
+                print(parts.get_content_type())
+                if parts.get_content_type() == 'text/plain':
+                    myMSG=base64.urlsafe_b64decode(parts.get_payload().encode('utf-8')).decode()
+                    print(myMSG)
+                if parts.get_content_type() == 'text/html':
+                    myMSG = BeautifulSoup(parts.get_payload().encode('iso-8859-1'), 'html.parser').text
+                    print(myMSG)
 
-            if char_set.lower() != 'iso-8859-1':
-                mime_msg = email.message_from_string(msg_str.decode(char_set))
 
-            if mime_msg.is_multipart():
-                body = mime_msg.get_payload()[0].get_payload()
-            else:
-                body = mime_msg.get_payload()
-
-            print(body)
-
+            # if mime_msg.is_multipart():
+            #     part = mime_msg.get_payload()[0]
+            #     char_set = part['Content-Type'].split('charset=')[-1].split(';')[0].strip('"').lower()
+            # else:
+            #     char_set = mime_msg['Content-Type'].split('charset=')[-1].split(';')[0].strip('"').lower()
+            #
+            # if mime_msg.is_multipart():
+            #     body = mime_msg.get_payload()[0].get_payload()
+            # else:
+            #     body = mime_msg.get_payload()
+            #
+            # print(body)
+            # print(char_set)
+            #
+            # if char_set.lower() != 'iso-8859-1':
+            #     mime_msg = email.message_from_string(msg_str.decode(char_set))
+            #
+            # if mime_msg.is_multipart():
+            #     body = mime_msg.get_payload()[0].get_payload()
+            # else:
+            #     body = mime_msg.get_payload()
+            #
+            # print(body)
+            #
 
            # for part in msg['payload']['parts']:
             #    print(part['body'])
