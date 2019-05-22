2a348222a9674731d9de9a48ce9dcd80de5258a9
Elise LARUELLE <laruelle@biocpc05.ens.fr>
2019-05-22 13:42:44 +0200

    update check reminder function#

--- semilandar.py
+++ semilandar.py
@@ -102,7 +102,56 @@ def process_event():
 # End process event
 
 # Function to check reminder
-def check_reminder():
+def check_reminder(event, service):
+    import Levenshtein
+    # use pip install python-Levenshtein
+    events_result = service.events().list(calendarId=team_calendar, timeMin=now, singleEvents=True,
+                                        orderBy='startTime').execute()#'p6o50l43pqssqamaip3kdb3k4g@group.calendar.google.com'
+	for ev in events_results:
+    	strDist = Levenshtein.distance(event['summary'], ev['summary'])
+		pourcentageSimilarities = 100 *strDist/float(len(event['summary']))
+		if pourcentageSimilarities >= 80. :
+			flagLoc = 0
+			flagDate = 0
+			flagTimeStart = 0
+			locDist = Levenshtein.distance(event['location'], ev['location'])
+			locPourcentageSimilarities = 100 * locDist/float(len(event['location']))
+			if locPourcentageSimilarities <= 80. :
+				print("location change from : " +ev['location'] + " to : " +event['location'])
+				#ev['location'] = event['location'] 
+				flagLoc = 1
+			dateDist = Levenshtein.distance(event['dateTime'], ev['locationTime'])
+			if dateDist != 0 :
+				print("start dateTime change from : " +ev['start']['dateTime'] + " to : " +event['start']['dateTime'])
+				#ev['start']['dateTime'] = event['start']['dateTime']
+
+				flagDate = 1
+				startEvent = datetime.datetime.fromisoformat(event['start']['dateTime'])
+				start_dateEvent = startEvent.strftime("%Y-%m-%d")
+				start_timeEvent = startEvent.strftime("%H:%M:%S")
+
+
+				startEv = datetime.datetime.fromisoformat(ev['start']['dateTime'])
+				start_dateEv = startEv.strftime("%Y-%m-%d")
+				start_timeEv = startEv.strftime("%H:%M:%S")
+
+				endEv = datetime.datetime.fromisoformat(ev['end']['dateTime'])
+				end_dateEv = endEv.strftime("%Y-%m-%d")
+				end_timeEv = endEv.strftime("%H:%M:%S")
+
+				formatTime = '%H:%M:%S'
+				evDuration = datetime.strptime(end_timeEv, formatTime) - datetime.strptime(start_timeEv, formatTime)
+				evDurationH = datetime.strftime(evDuration ,'%H')
+				evDurationM = datetime.strftime(evDuration ,'%M')
+
+
+
+
+				print("end dateTime change")
+				event['end']['dateTime'] = event['start']['dateTime'] + datetime.detlatime(minutes= evDurationM,hours = evDurationH )
+				event['end']['timeZone'] = 'Europe/Paris'
+			service.events().update(calendarId=team_calendar,eventId = ev['id'], body = event).execute()
+			
     return None
 # End check reminder
 
@@ -113,5 +162,6 @@ def create_new_event():
 
 # Function update event
 def update_event():
+
     return None
 # End update event
