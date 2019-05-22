f6ad76063645b47f1acaa2e450965d10fb70d092
corbe <corbe@biologie.ens.fr>
2019-05-22 13:52:33 +0200

    new version

--- bin/readEmail.py
+++ bin/readEmail.py
@@ -2,8 +2,6 @@ from googleapiclient.discovery import build
 from httplib2 import Http
 from oauth2client import file, client, tools
 import base64
-import quopri
-import email
 from bs4 import BeautifulSoup
 
 SCOPES = 'https://www.googleapis.com/auth/gmail.readonly'
@@ -18,7 +16,9 @@ def main():
     service = build('gmail', 'v1', http=creds.authorize(Http()))
 
     # Call the Gmail API to fetch INBOX
-    results = service.users().messages().list(userId='me', labelIds=['INBOX']).execute()
+    results = service.users().messages().list(
+        userId='me'
+    ).execute()
     messages = results.get('messages', [])
 
     if not messages:
@@ -26,53 +26,33 @@ def main():
 
     else:
 
-        for message in messages[:5]:
-            msg = service.users().messages().get(userId='me', id=message['id'], format='raw').execute()
+        for message in messages:
+            msg = service.users().messages().get(userId='me',
+                                                 id=message['id'],
+                                                 format="full").execute()
+            result = msg['payload']
 
-            # base 62 renvoie un string encodé, mais la fonction par défaut de python ne peut pas le décoder...
-            msg_str = base64.urlsafe_b64decode(msg['raw'])
+            if "parts" in result:  # multipart email (rare)
+                result = [c for c in result['parts'] if 'data' in c['body']]
+                # get body from all part
+                mimes = [c['mimeType'] for c in result]
+                # get mimetype from all part
+                try:
+                    result = result[mimes.index('text/plain')]
+                    mime = 'text/plain'
+                    # try to get body in plain text
+                except (IndexError, ValueError):
+                    result = result[0]  # fallback to first if not
+                    mime = mimes[0]
+            else:
+                mime = result['mimeType']
 
-            mime_msg = email.message_from_string(msg_str.decode('iso-8859-1'))
+            content = base64.urlsafe_b64decode(result['body']['data'])
 
-
-            for parts in mime_msg.walk():
-                mime_msg.get_payload()
-                print(parts.get_content_type())
-                if parts.get_content_type() == 'text/plain':
-                    myMSG=base64.urlsafe_b64decode(parts.get_payload().encode('utf-8')).decode()
-                    print(myMSG)
-                if parts.get_content_type() == 'text/html':
-                    myMSG = BeautifulSoup(parts.get_payload().encode('iso-8859-1'), 'html.parser').text
-                    print(myMSG)
-
-
-            # if mime_msg.is_multipart():
-            #     part = mime_msg.get_payload()[0]
-            #     char_set = part['Content-Type'].split('charset=')[-1].split(';')[0].strip('"').lower()
-            # else:
-            #     char_set = mime_msg['Content-Type'].split('charset=')[-1].split(';')[0].strip('"').lower()
-            #
-            # if mime_msg.is_multipart():
-            #     body = mime_msg.get_payload()[0].get_payload()
-            # else:
-            #     body = mime_msg.get_payload()
-            #
-            # print(body)
-            # print(char_set)
-            #
-            # if char_set.lower() != 'iso-8859-1':
-            #     mime_msg = email.message_from_string(msg_str.decode(char_set))
-            #
-            # if mime_msg.is_multipart():
-            #     body = mime_msg.get_payload()[0].get_payload()
-            # else:
-            #     body = mime_msg.get_payload()
-            #
-            # print(body)
-            #
-
-           # for part in msg['payload']['parts']:
-            #    print(part['body'])
+            if mime == 'text/html':
+                content = BeautifulSoup(content.decode('utf-8'),
+                                        'html.parser').text
+            print(content)
 
 
 
